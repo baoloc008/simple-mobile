1d31f7c047f60aab5ca9e44e02fad36fd24a6531
Loc <baoloc008@gmail.com>
2018-03-26 05:29:21 +0700

    play video

--- app/src/main/java/com/simple/youtuberemote/MainActivity.java
+++ app/src/main/java/com/simple/youtuberemote/MainActivity.java
@@ -1,13 +1,48 @@
 package com.simple.youtuberemote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+    String API_KEY = "AIzaSyCf2Nk7-x-4l3nmcKnPgqTZ9G_uRHkhhSs";
+    YouTubePlayerView youTubePlayerView;
+    int REQUEST_CODE = 2212;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        youTubePlayerView = findViewById(R.id.youtubeplayerview);
+        youTubePlayerView.initialize(API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.cueVideo("EJxoLwcCx8E");
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(MainActivity.this, REQUEST_CODE);
+        }else {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            youTubePlayerView.initialize(API_KEY, MainActivity.this);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
