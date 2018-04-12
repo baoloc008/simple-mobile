7d72124440ca4bc17acbfc7d6c2b6f64dc71c747
Nguyen Minh Trung <tuxdomatna@gmail.com>
2018-04-12 23:53:32 +0700

    format code

--- app/src/main/java/com/simple/youtuberemote/PlayerActivity.java
+++ app/src/main/java/com/simple/youtuberemote/PlayerActivity.java
@@ -7,25 +7,25 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class PlayerActivity extends YouTubeFailureRecoveryActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_player);
 
-        YouTubePlayerView youTubeView = findViewById(R.id.youtube_view);
-        youTubeView.initialize("AIzaSyDiUkq0Jx96EW-D-zuqR45KYIvDmAnPn1s", this);
-    }
+    YouTubePlayerView youTubeView = findViewById(R.id.youtube_view);
+    youTubeView.initialize("AIzaSyDiUkq0Jx96EW-D-zuqR45KYIvDmAnPn1s", this);
+  }
 
-    @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
-                                        boolean wasRestored) {
-        if (!wasRestored) {
-            player.cueVideo("wKJ9KzGQq0w");
-        }
+  @Override
+  public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                      boolean wasRestored) {
+    if (!wasRestored) {
+      player.cueVideo("wKJ9KzGQq0w");
     }
+  }
 
-    @Override
-    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
-        return (YouTubePlayerView) findViewById(R.id.youtube_view);
-    }
+  @Override
+  protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+    return (YouTubePlayerView) findViewById(R.id.youtube_view);
+  }
 }
--- app/src/main/java/com/simple/youtuberemote/YouTubeFailureRecoveryActivity.java
+++ app/src/main/java/com/simple/youtuberemote/YouTubeFailureRecoveryActivity.java
@@ -1,11 +1,11 @@
 package com.simple.youtuberemote;
 
+import android.content.Intent;
+import android.widget.Toast;
+
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-
-import android.content.Intent;
-import android.widget.Toast;
 
 /**
  * An abstract activity which deals with recovering from errors which may occur during API
@@ -14,27 +14,27 @@ import android.widget.Toast;
 public abstract class YouTubeFailureRecoveryActivity extends YouTubeBaseActivity implements
         YouTubePlayer.OnInitializedListener {
 
-    private static final int RECOVERY_DIALOG_REQUEST = 1;
+  private static final int RECOVERY_DIALOG_REQUEST = 1;
 
-    @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider,
-                                        YouTubeInitializationResult errorReason) {
-        if (errorReason.isUserRecoverableError()) {
-            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
-        } else {
-            String errorMessage = String.format("Có lỗi xảy ra: (%1$s)", errorReason.toString());
-            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
-        }
+  @Override
+  public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                      YouTubeInitializationResult errorReason) {
+    if (errorReason.isUserRecoverableError()) {
+      errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+    } else {
+      String errorMessage = String.format("Có lỗi xảy ra: (%1$s)", errorReason.toString());
+      Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
     }
+  }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RECOVERY_DIALOG_REQUEST) {
-            // Retry initialization if user performed a recovery action
-            getYouTubePlayerProvider().initialize("AIzaSyDiUkq0Jx96EW-D-zuqR45KYIvDmAnPn1s", this);
-        }
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == RECOVERY_DIALOG_REQUEST) {
+      // Retry initialization if user performed a recovery action
+      getYouTubePlayerProvider().initialize("AIzaSyDiUkq0Jx96EW-D-zuqR45KYIvDmAnPn1s", this);
     }
+  }
 
-    protected abstract YouTubePlayer.Provider getYouTubePlayerProvider();
+  protected abstract YouTubePlayer.Provider getYouTubePlayerProvider();
 
 }
