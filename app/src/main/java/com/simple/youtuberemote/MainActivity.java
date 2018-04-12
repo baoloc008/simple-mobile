32cfbb6902ccace14fa12bb98e175a52f5e0d9e4
Nguyen Minh Trung <tuxdomatna@gmail.com>
2018-04-12 23:53:02 +0700

    update MainActivity UI

--- app/src/main/java/com/simple/youtuberemote/MainActivity.java
+++ app/src/main/java/com/simple/youtuberemote/MainActivity.java
@@ -1,25 +1,25 @@
 package com.simple.youtuberemote;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        Button playBtn = findViewById(R.id.play_btn);
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.this.startActivity(new Intent(MainActivity.this, PlayerActivity.class));
-            }
-        });
-    }
+    Button playBtn = findViewById(R.id.tv_btn);
+    playBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        MainActivity.this.startActivity(new Intent(MainActivity.this, PlayerActivity.class));
+      }
+    });
+  }
 }
