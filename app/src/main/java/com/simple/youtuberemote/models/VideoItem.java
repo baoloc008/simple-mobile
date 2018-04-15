dadf6346a5b5ddd7b9db4ea90442ae622f07046e
Loc <baoloc008@gmail.com>
2018-04-15 16:29:16 +0700

    add VideoItem model

---
+++ app/src/main/java/com/simple/youtuberemote/models/VideoItem.java
@@ -0,0 +1,51 @@
+package com.simple.youtuberemote.models;
+
+/**
+ * Created by loc on 15/04/2018.
+ */
+
+public class VideoItem {
+  private String thumbnail, title, subTitle, videoId;
+
+  public VideoItem() {
+  }
+
+  public VideoItem(String thumbnail, String title, String subTitle, String videoId) {
+    this.thumbnail = thumbnail;
+    this.title = title;
+    this.subTitle = subTitle;
+    this.videoId = videoId;
+  }
+
+  public String getThumbnail() {
+    return thumbnail;
+  }
+
+  public void setThumbnail(String thumbnail) {
+    this.thumbnail = thumbnail;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getSubTitle() {
+    return subTitle;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+
+  public String getVideoId() {
+    return videoId;
+  }
+
+  public void setVideoId(String videoId) {
+    this.videoId = videoId;
+  }
+}
