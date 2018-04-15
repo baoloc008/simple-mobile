7c843b479fd2b9c6bf8bff0650c0b961cfaf1977
Nguyễn Minh Trung <tuxdomatna@gmail.com>
2018-04-15 22:54:24 +0700

    add listener client

--- app/src/main/java/com/simple/youtuberemote/networks/Client.java
+++ app/src/main/java/com/simple/youtuberemote/networks/Client.java
@@ -1,16 +1,43 @@
 package com.simple.youtuberemote.networks;
 
+import com.simple.youtuberemote.models.message.Message;
+import com.simple.youtuberemote.models.message.PlayList;
+
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class Client {
   private Socket socket;
   private ArrayList<String> playList;
+  private String currentVideo;
 
   public Client(String ip) {
     try {
       socket = new Socket(ip, Server.PORT);
+      new Thread(new Runnable() {
+        @Override
+        public void run(){
+          while (true) {
+            try {
+              ObjectInputStream streamIn = new ObjectInputStream(socket.getInputStream());
+              Message message = (Message) streamIn.readObject();
+              switch (message.type) {
+                case PLAY_LIST:
+                  PlayList data = (PlayList) message.data;
+                  playList = data.playlist;
+                  currentVideo = data.currentVideo;
+                  break;
+                default:
+                  break;
+              }
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      }).start();
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -18,5 +45,9 @@ public class Client {
 
   public ArrayList<String> getPlayList() {
     return playList;
+  }
+
+  public String getCurrentVideo() {
+    return currentVideo;
   }
 }
