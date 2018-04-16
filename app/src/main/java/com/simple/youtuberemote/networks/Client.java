b30b49e486663e5f987eb3fbaab61f77669aee46
Nguyễn Minh Trung <tuxdomatna@gmail.com>
2018-04-16 20:36:38 +0700

    update client

--- app/src/main/java/com/simple/youtuberemote/networks/Client.java
+++ app/src/main/java/com/simple/youtuberemote/networks/Client.java
@@ -1,53 +1,70 @@
 package com.simple.youtuberemote.networks;
 
+import com.simple.youtuberemote.models.message.AddVideo;
 import com.simple.youtuberemote.models.message.Message;
 import com.simple.youtuberemote.models.message.PlayList;
+import com.simple.youtuberemote.models.message.PlaySpecVideo;
+import com.simple.youtuberemote.models.message.Type;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
+
 public class Client {
-  private Socket socket;
+  private Socket            socket;
   private ArrayList<String> playList;
-  private String currentVideo;
+  private String            currentVideo;
 
   public Client(String ip) {
     try {
       socket = new Socket(ip, Server.PORT);
-      new Thread(new Runnable() {
+      (new Listener(socket) {
         @Override
-        public void run(){
-          while (true) {
-            try {
-              ObjectInputStream streamIn = new ObjectInputStream(socket.getInputStream());
-              Message message = (Message) streamIn.readObject();
-              switch (message.type) {
-                case PLAY_LIST:
-                  PlayList data = (PlayList) message.data;
-                  playList = data.playlist;
-                  currentVideo = data.currentVideo;
-                  break;
-                default:
-                  break;
-              }
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
+        public void onMessage(Message message) {
+          switch (message.type) {
+            case PLAY_LIST:
+              PlayList data = (PlayList) message.data;
+              playList = data.playlist;
+              currentVideo = data.currentVideo;
+              break;
+            default:
+              break;
           }
         }
       }).start();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  private void send(Socket client, Message message) {
+    try {
+      ObjectOutputStream streamOut = new ObjectOutputStream(client.getOutputStream());
+      streamOut.writeObject(message);
+      streamOut.flush();
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
-
   public ArrayList<String> getPlayList() {
     return playList;
   }
 
   public String getCurrentVideo() {
     return currentVideo;
+  }
+  public void pauseVideo() {
+    send(socket, new Message(Type.PAUSE, null));
+  }
+  public void playVideo() {
+    send(socket, new Message(Type.PLAY, null));
+  }
+  public void playVideo(String id) {
+    send(socket, new Message(Type.PLAY_SPEC, new PlaySpecVideo(id)));
+  }
+  public void addVideo(String id) {
+    send(socket, new Message(Type.ADD_VIDEO, new AddVideo(id)));
   }
 }
