d0519bf293fc1d3c0656a5dfb190a0f519c854e8
Nguyễn Minh Trung <tuxdomatna@gmail.com>
2018-04-16 20:36:06 +0700

    add PlaySpecVideo Data

---
+++ app/src/main/java/com/simple/youtuberemote/models/message/PlaySpecVideo.java
@@ -0,0 +1,9 @@
+package com.simple.youtuberemote.models.message;
+
+public class PlaySpecVideo extends Data {
+  public String videoId;
+
+  public PlaySpecVideo(String id) {
+    videoId = id;
+  }
+}
