f472e80fddce260cbe55b2008b43b392911d4301
Nguyễn Minh Trung <tuxdomatna@gmail.com>
2018-04-16 20:36:23 +0700

    create Listener class

---
+++ app/src/main/java/com/simple/youtuberemote/networks/Listener.java
@@ -0,0 +1,32 @@
+package com.simple.youtuberemote.networks;
+
+import android.util.Log;
+
+import com.simple.youtuberemote.models.message.Message;
+
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+
+public abstract class Listener extends Thread{
+  private Socket socket;
+
+  public Listener(Socket _socket) {
+    socket = _socket;
+  }
+  public void run() {
+    while (true) {
+      try {
+        ObjectInputStream streamIn = new ObjectInputStream(socket.getInputStream());
+        Message message  = (Message) streamIn.readObject();
+        Log.d("Message", message.toString());
+        onMessage(message);
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  public abstract void onMessage(Message message);
+
+}
