3b97d96b89a584838b44bedaeb12f610097e427f
Tho Nguyen Duc <ndtho8205@gmail.com>
2018-04-24 00:16:32 +0700

    Update video_item layout.

--- app/src/main/java/com/simple/youtuberemote/fragments/HomeFragment.java
+++ app/src/main/java/com/simple/youtuberemote/fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -59,6 +60,8 @@ public class HomeFragment extends Fragment
     RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
     homeAdapter = new HomeAdapter(getActivity(), videoList);
     recyclerViewHome.setLayoutManager(layoutManager);
+    recyclerViewHome.addItemDecoration(new DividerItemDecoration(getContext(),
+                                                                 DividerItemDecoration.VERTICAL));
     recyclerViewHome.setAdapter(homeAdapter);
 
     dataClient = APIUtils.getData();
