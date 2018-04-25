eed68f6ace164e80021db7b122399bf72309fa7c
Đinh Bảo Lộc <baoloc008@gmail.com>
2018-04-25 16:46:45 +0700

    add dialog to HomeFragment

--- app/src/main/java/com/simple/youtuberemote/fragments/HomeFragment.java
+++ app/src/main/java/com/simple/youtuberemote/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.simple.youtuberemote.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,9 +12,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.simple.youtuberemote.R;
+import com.simple.youtuberemote.activities.RemoteControlActivity;
 import com.simple.youtuberemote.adapters.HomeAdapter;
 import com.simple.youtuberemote.models.API.searchvideos.Item;
 import com.simple.youtuberemote.models.API.searchvideos.SearchVideos;
@@ -27,6 +31,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -36,7 +41,7 @@ import retrofit2.Response;
  * Created by loc on 15/04/2018.
  */
 
-public class HomeFragment extends Fragment
+public class HomeFragment extends Fragment implements HomeAdapter.RecyclerViewHomeAdapterOnclickListener
 {
 
   @BindView (R.id.recycleViewHome)
@@ -45,7 +50,9 @@ public class HomeFragment extends Fragment
   private HomeAdapter     homeAdapter;
   private List<VideoItem> videoList;
   private DataClient      dataClient;
-
+  private AlertDialog.Builder mBuilder;
+  private AlertDialog mAlertDialog;
+  private TextView txtvClickPlay, txtvClickAddPlaylist;
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater,
@@ -58,7 +65,7 @@ public class HomeFragment extends Fragment
     videoList = new ArrayList<>();
 
     RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-    homeAdapter = new HomeAdapter(getActivity(), videoList);
+    homeAdapter = new HomeAdapter(getActivity(), videoList, this);
     recyclerViewHome.setLayoutManager(layoutManager);
     recyclerViewHome.addItemDecoration(new DividerItemDecoration(getContext(),
                                                                  DividerItemDecoration.VERTICAL));
@@ -140,6 +147,43 @@ public class HomeFragment extends Fragment
       }
     });
 
+    mBuilder = new AlertDialog.Builder(getActivity());
+    View dialogView = getLayoutInflater().inflate(R.layout.dialog_video_option, null);
+    mBuilder.setView(dialogView);
+    txtvClickPlay = dialogView.findViewById(R.id.txtvPlay);
+    txtvClickAddPlaylist = dialogView.findViewById(R.id.txtvAddToPlaylist);
+
+    mAlertDialog = mBuilder.create();
     return view;
+  }
+
+  @Override
+  public void onItemClick(View v, int position)
+  {
+    final VideoItem videoItem = videoList.get(position);
+    Toast.makeText(getActivity(), videoItem.getVideoId(), Toast.LENGTH_SHORT).show();
+
+    txtvClickPlay.setOnClickListener(new View.OnClickListener()
+    {
+      @Override
+      public void onClick(View view)
+      {
+        Toast.makeText(getActivity(), "click play", Toast.LENGTH_SHORT).show();
+        mAlertDialog.dismiss();
+        RemoteControlActivity.mClient.playVideo(videoItem.getVideoId());
+      }
+    });
+    txtvClickAddPlaylist.setOnClickListener(new View.OnClickListener()
+    {
+      @Override
+      public void onClick(View view)
+      {
+        Toast.makeText(getActivity(), "click add playlist", Toast.LENGTH_SHORT).show();
+        mAlertDialog.dismiss();
+        RemoteControlActivity.mClient.addVideo(videoItem.getVideoId());
+      }
+    });
+
+    mAlertDialog.show();
   }
 }
