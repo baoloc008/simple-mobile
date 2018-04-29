ef80d61f7518934e066901852b049690cb136963
Nguyễn Minh Trung <tuxdomatna@gmail.com>
2018-04-29 16:26:40 +0700

    update HomeFragment, use Youtube API

--- app/src/main/java/com/simple/youtuberemote/fragments/HomeFragment.java
+++ app/src/main/java/com/simple/youtuberemote/fragments/HomeFragment.java
@@ -10,12 +10,15 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
 import com.simple.youtuberemote.R;
 import com.simple.youtuberemote.activities.RemoteControlActivity;
 import com.simple.youtuberemote.activities.SearchActivity;
@@ -25,6 +28,9 @@ import com.simple.youtuberemote.models.API.searchvideos.Item;
 import com.simple.youtuberemote.models.API.searchvideos.SearchVideos;
 import com.simple.youtuberemote.models.API.videodetail.VideoDetail;
 import com.simple.youtuberemote.models.VideoItem;
+import com.simple.youtuberemote.networks.YoutubeApi.FetchVideoDetailTask;
+import com.simple.youtuberemote.networks.YoutubeApi.SearchYoutubeTask;
+import com.simple.youtuberemote.networks.YoutubeApi.YoutubeApiHelper;
 import com.simple.youtuberemote.networks.retrofit.APIUtils;
 import com.simple.youtuberemote.networks.retrofit.DataClient;
 import com.simple.youtuberemote.utils.VideoPopupMenuOnItemClickHandler;
@@ -46,24 +52,45 @@ import retrofit2.Response;
  */
 
 public class HomeFragment extends Fragment
-    implements HomeAdapter.RecyclerViewHomeAdapterOnclickListener
+    implements RecyclerArrayAdapter.OnLoadMoreListener
 {
-
-  @BindView (R.id.rv_home_videos)
-  RecyclerView mVideoList;
-
   @BindView (R.id.tv_empty_message)
   TextView mEmptyMessage;
 
   @BindView (R.id.fab_search)
   FloatingActionButton mSearchButton;
 
-  private VideoListAdapter    homeAdapter;
-  private List<VideoItem>     videoList;
-  private DataClient          dataClient;
-  private AlertDialog.Builder mBuilder;
-  private AlertDialog         mAlertDialog;
-  private TextView            txtvClickPlay, txtvClickAddPlaylist;
+  @BindView (R.id.rv_home_video)
+  EasyRecyclerView mResultVideoList;
+
+  private VideoListAdapter mResultVideoListAdapter;
+  private List<VideoItem> mSearchResults;
+  private Boolean mIsNewSearch = false;
+
+  private SearchYoutubeTask    mSearchTask = YoutubeApiHelper.searchYoutube();
+  private FetchVideoDetailTask mFetchTask  = YoutubeApiHelper.fetchVideoDetail();
+
+  private SearchYoutubeTask.Callback mSearchTaskCallback
+      = new SearchYoutubeTask.Callback()
+  {
+
+    @Override
+    public void onSearchComplete(boolean ok, List<String> result)
+    {
+      if (ok) {
+        mSearchResults = mFetchTask.fetch(result);
+        if (mIsNewSearch) {
+          mResultVideoListAdapter.clear();
+          mIsNewSearch = false;
+        }
+        mResultVideoListAdapter.addAll(mSearchResults);
+      }
+      else {
+        mResultVideoListAdapter.pauseMore();
+        mResultVideoList.showError();
+      }
+    }
+  };
 
   @Nullable
   @Override
@@ -73,133 +100,22 @@ public class HomeFragment extends Fragment
   {
     View view = inflater.inflate(R.layout.fragment_home, container, false);
     ButterKnife.bind(this, view);
+    initResultVideoListView();
 
-    videoList = new ArrayList<>();
-
-    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-    homeAdapter = new VideoListAdapter(getContext(), VideoListAdapter.FULL_VIEW_TYPE,
-                                       new VideoPopupMenuOnItemClickHandler(getContext()));
-    mVideoList.setLayoutManager(layoutManager);
-    mVideoList.addItemDecoration(new DividerItemDecoration(getContext(),
-                                                           DividerItemDecoration.VERTICAL));
-    mVideoList.setAdapter(homeAdapter);
-
-    dataClient = APIUtils.getData();
-    dataClient.getSearchVideos("phim songoku").enqueue(new Callback<SearchVideos>()
-    {
-      @Override
-      public void onResponse(@NonNull Call<SearchVideos> call,
-                             @NonNull Response<SearchVideos> response)
-      {
-        if (response.isSuccessful()) {
-          SearchVideos searchVideos = response.body();
-          if (searchVideos != null) {
-            List<Item> videoAPIList = searchVideos.getItems();
-            for (final Item item : videoAPIList) {
-              final VideoItem videoItem = new VideoItem();
-              videoItem.setThumbnailUrl(item.getSnippet().getThumbnails().getMedium().getUrl());
-              videoItem.setTitle(item.getSnippet().getTitle());
-              videoItem.setVideoId(item.getId().getVideoId());
-              dataClient.getVideoDetail(item.getId().getVideoId())
-                        .enqueue(new Callback<VideoDetail>()
-                        {
-                          @Override
-                          public void onResponse(@NonNull Call<VideoDetail> call,
-                                                 @NonNull Response<VideoDetail> response)
-                          {
-                            if (response.isSuccessful()) {
-                              VideoDetail videoDetail = response.body();
-                              String duration = videoDetail.getItems()
-                                                           .get(0)
-                                                           .getContentDetails()
-                                                           .getDuration();
-                              videoItem.setDuration(duration);
-                              videoItem.setViewCount(new BigInteger(videoDetail.getItems()
-                                                                               .get(0)
-                                                                               .getStatistics()
-                                                                               .getViewCount()));
-                              videoItem.setChannelTitle(item.getSnippet().getChannelTitle());
-                              homeAdapter.add(videoItem);
-                            }
-                            else {
-                              Toast.makeText(getActivity(),
-                                             String.valueOf("Error2:" + response.code()),
-                                             Toast.LENGTH_SHORT).show();
-                            }
-                          }
-
-                          @Override
-                          public void onFailure(@NonNull Call<VideoDetail> call,
-                                                @NonNull Throwable t)
-                          {
-                            Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT)
-                                 .show();
-                          }
-                        });
-              videoList.add(videoItem);
-
-//              homeAdapter.notifyDataSetChanged();
-            }
-          }
-        }
-        else {
-          Toast.makeText(getActivity(),
-                         String.valueOf("Error 1:" + response.code()),
-                         Toast.LENGTH_SHORT).show();
-        }
-      }
-
-      @Override
-      public void onFailure(@NonNull Call<SearchVideos> call, @NonNull Throwable t)
-      {
-        Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
-      }
-    });
-
-    mBuilder = new AlertDialog.Builder(getActivity());
-    View dialogView = getLayoutInflater().inflate(R.layout.dialog_video_option, null);
-    mBuilder.setView(dialogView);
-    txtvClickPlay = dialogView.findViewById(R.id.txtvPlay);
-    txtvClickAddPlaylist = dialogView.findViewById(R.id.txtvAddToPlaylist);
-
-    mAlertDialog = mBuilder.create();
     return view;
   }
 
   @Override
-  public void onItemClick(View v, int position)
+  public void onStart()
   {
-    final VideoItem videoItem = videoList.get(position);
-    txtvClickPlay.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        mAlertDialog.dismiss();
-        try {
-          RemoteControlActivity.mClient.playVideo(videoItem.getVideoId());
-        }
-        catch (Exception e) {
-          Toast.makeText(getActivity(), "Chưa kết nối đến TV", Toast.LENGTH_SHORT).show();
-        }
-      }
-    });
-    txtvClickAddPlaylist.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        mAlertDialog.dismiss();
-        try {
-          RemoteControlActivity.mClient.addVideo(videoItem.getVideoId());
-        }
-        catch (Exception e) {
-          Toast.makeText(getActivity(), "Chưa kết nối đến TV", Toast.LENGTH_SHORT).show();
-        }
-      }
-    });
+    super.onStart();
+    mSearchTask.searchAsync("karaoke", mSearchTaskCallback);
+  }
 
-    mAlertDialog.show();
+  @Override
+  public void onLoadMore()
+  {
+    mSearchTask.searchNextAsync();
   }
 
   @OnClick (R.id.fab_search)
@@ -208,4 +124,36 @@ public class HomeFragment extends Fragment
     Intent intent = new Intent(getContext(), SearchActivity.class);
     startActivityForResult(intent, SearchActivity.SEARCH_REQUEST_CODE);
   }
+  private void initResultVideoListView()
+  {
+    mResultVideoListAdapter = new VideoListAdapter(getContext(), VideoListAdapter.COMPACT_VIEW_TYPE,
+                                                   new VideoPopupMenuOnItemClickHandler(getContext()));
+
+    LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(),
+                                                                            DividerItemDecoration.VERTICAL);
+    mResultVideoList.setLayoutManager(layoutManager);
+    mResultVideoList.addItemDecoration(dividerItemDecoration);
+    mResultVideoList.setAdapterWithProgress(mResultVideoListAdapter);
+
+    mResultVideoListAdapter.setMore(R.layout.rv_more, this);
+    mResultVideoListAdapter.setError(R.layout.rv_error, new RecyclerArrayAdapter.OnErrorListener()
+    {
+      @Override
+      public void onErrorShow()
+      {
+        mResultVideoListAdapter.resumeMore();
+      }
+
+      @Override
+      public void onErrorClick()
+      {
+        mResultVideoListAdapter.resumeMore();
+      }
+    });
+
+    mResultVideoListAdapter.clear();
+    mResultVideoListAdapter.pauseMore();
+  }
+
 }
