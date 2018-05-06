d568b2678a2a37841ac9b9c417698ca39e7f36ca
Unknown <tuxdomatna@gmail.com>
2018-05-06 12:15:57 +0700

    add RemoveVideo Message

---
+++ app/src/main/java/com/simple/youtuberemote/models/message/RemoveVideo.java
@@ -0,0 +1,10 @@
+package com.simple.youtuberemote.models.message;
+
+public class RemoveVideo extends Data
+{
+  public String videoId;
+
+  public RemoveVideo(String id) {
+    videoId = id;
+  }
+}
