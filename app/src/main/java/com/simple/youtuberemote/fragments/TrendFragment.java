0ef77ed0ece29194aaa2a4fe361ac08eec3313eb
Đinh Bảo Lộc <baoloc008@gmail.com>
2018-05-06 16:22:43 +0700

    call api to get trend video

--- app/src/main/java/com/simple/youtuberemote/fragments/TrendFragment.java
+++ app/src/main/java/com/simple/youtuberemote/fragments/TrendFragment.java
@@ -3,16 +3,24 @@ package com.simple.youtuberemote.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
 import com.simple.youtuberemote.R;
 import com.simple.youtuberemote.adapters.HomeAdapter;
 import com.simple.youtuberemote.adapters.TrendAdapter;
+import com.simple.youtuberemote.adapters.VideoListAdapter.VideoListAdapter;
 import com.simple.youtuberemote.models.VideoItem;
+import com.simple.youtuberemote.networks.YoutubeApi.FetchVideoDetailTask;
+import com.simple.youtuberemote.networks.YoutubeApi.GetTrendVideoTask;
+import com.simple.youtuberemote.networks.YoutubeApi.YoutubeApiHelper;
+import com.simple.youtuberemote.utils.VideoPopupMenuOnItemClickHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,12 +33,34 @@ import butterknife.ButterKnife;
  * Created by loc on 15/04/2018.
  */
 
-public class TrendFragment extends Fragment
+public class TrendFragment extends Fragment implements RecyclerArrayAdapter.OnLoadMoreListener
 {
-  private TrendAdapter    trendAdapter;
-  private List<VideoItem> videoList;
-  @BindView (R.id.recycleViewTrend)
-  RecyclerView recyclerViewHome;
+  @BindView (R.id.rv_trend_video)
+  EasyRecyclerView mRecyclerViewTrendVideo;
+
+  private VideoListAdapter mVideoListAdapter;
+  private List<VideoItem> mVideoItems;
+
+  private GetTrendVideoTask mGetTrendVideoTask = YoutubeApiHelper.getTrendVideoTask();
+  private FetchVideoDetailTask mFetchTask  = YoutubeApiHelper.fetchVideoDetail();
+
+  private GetTrendVideoTask.Callback mGetTrendVideoCallback = new GetTrendVideoTask.Callback()
+  {
+    @Override
+    public void onGetTrendVideoComplete(boolean ok, List<String> result)
+    {
+      if (ok)
+      {
+        mVideoItems = mFetchTask.fetch(result);
+        mVideoListAdapter.addAll(mVideoItems);
+      }
+      else
+      {
+        mVideoListAdapter.pauseMore();
+        mRecyclerViewTrendVideo.showError();
+      }
+    }
+  };
 
   @Nullable
   @Override
@@ -40,11 +70,52 @@ public class TrendFragment extends Fragment
   {
     View view = inflater.inflate(R.layout.fragment_trend, container, false);
     ButterKnife.bind(this, view);
-    videoList = new ArrayList<>();
-    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-    trendAdapter = new TrendAdapter(getActivity(), videoList);
-    recyclerViewHome.setLayoutManager(layoutManager);
-    recyclerViewHome.setAdapter(trendAdapter);
+    initTrendVideoListView();
+
     return view;
+  }
+
+  private void initTrendVideoListView()
+  {
+    mVideoListAdapter = new VideoListAdapter(getContext(),
+                                             VideoListAdapter.COMPACT_VIEW_TYPE,
+                                             new VideoPopupMenuOnItemClickHandler(getContext()));
+    LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+    mRecyclerViewTrendVideo.setLayoutManager(layoutManager);
+    mRecyclerViewTrendVideo.addItemDecoration(dividerItemDecoration);
+    mRecyclerViewTrendVideo.setAdapterWithProgress(mVideoListAdapter);
+
+    mVideoListAdapter.setMore(R.layout.rv_more, this);
+    mVideoListAdapter.setError(R.layout.rv_error, new RecyclerArrayAdapter.OnErrorListener()
+    {
+      @Override
+      public void onErrorShow()
+      {
+        mVideoListAdapter.resumeMore();
+      }
+
+      @Override
+      public void onErrorClick()
+      {
+        mVideoListAdapter.resumeMore();
+      }
+    });
+
+    mVideoListAdapter.clear();
+    mVideoListAdapter.pauseMore();
+  }
+
+  @Override
+  public void onStart()
+  {
+    super.onStart();
+    mGetTrendVideoTask.getTrendVideoAsync(mGetTrendVideoCallback);
+  }
+
+  @Override
+  public void onLoadMore()
+  {
+    mGetTrendVideoTask.getNextTrendVideoAsync();
   }
 }
