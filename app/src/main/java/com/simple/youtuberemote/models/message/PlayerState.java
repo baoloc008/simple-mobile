2782c697f37981758afcea9e584f7b6c5fdef37d
Nguyễn Minh Trung <tuxdomatna@gmail.com>
2018-05-06 23:00:07 +0700

    add PlayerState

---
+++ app/src/main/java/com/simple/youtuberemote/models/message/PlayerState.java
@@ -0,0 +1,10 @@
+package com.simple.youtuberemote.models.message;
+
+public class PlayerState extends Data
+{
+  public boolean isPlaying;
+
+  public PlayerState(boolean _isPlaying) {
+    isPlaying = _isPlaying;
+  }
+}
