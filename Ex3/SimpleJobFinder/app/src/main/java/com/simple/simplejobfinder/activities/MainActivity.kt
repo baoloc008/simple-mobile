338526a1f6db9fe782deacbdbdb4eee8e61a261b
Loc <baoloc008@gmail.com>
2018-05-24 19:22:53 +0700

    push notification service

--- Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/activities/MainActivity.kt
+++ Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/activities/MainActivity.kt
@@ -2,14 +2,17 @@ package com.simple.simplejobfinder.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.iid.FirebaseInstanceId
 import com.simple.simplejobfinder.R
 
 class MainActivity : AppCompatActivity()
 {
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("token", FirebaseInstanceId.getInstance().token)
     }
 }
---
+++ Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/services/FirebaseMessagingService.kt
@@ -0,0 +1,42 @@
+package com.simple.simplejobfinder.services
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.simple.simplejobfinder.R
+import com.simple.simplejobfinder.activities.MainActivity
+
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        remoteMessage?.let {
+            val message = remoteMessage.notification?.body
+            val title = remoteMessage.notification?.title
+           message?.let {
+               pushNotification(title ?: "", message, false)
+           }
+        }
+    }
+
+    private fun pushNotification(title: String, content: String, isOngoing: Boolean) {
+        val builder = NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setOngoing(isOngoing)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(content))
+                .setAutoCancel(true)
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_ONE_SHOT)
+        builder.setContentIntent(contentIntent)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(0, builder.build())
+    }
+}
