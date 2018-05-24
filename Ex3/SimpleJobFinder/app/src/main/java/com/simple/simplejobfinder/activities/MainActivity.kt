daa60ddd6f73320e5e11ac7b1cc6b8a23e24a931
Loc <baoloc008@gmail.com>
2018-05-25 00:06:08 +0700

    demo get data from database

--- Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/activities/MainActivity.kt
+++ Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.simple.simplejobfinder.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.google.firebase.FirebaseApp
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.iid.FirebaseInstanceId
 import com.simple.simplejobfinder.R
 
@@ -14,5 +17,24 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("token", FirebaseInstanceId.getInstance().token)
+        val mData = FirebaseDatabase.getInstance().reference
+        mData.child("location").addChildEventListener(object: ChildEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                val location = p0.getValue(String::class.java)
+                Log.d("location", location)
+            }
+        })
     }
 }
