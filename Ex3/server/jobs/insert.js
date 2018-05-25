bed916aa10584839fb8c7288a4aa0d11851626b6
Nguyen Minh Trung <tuxdomatna@gmail.com>
2018-05-25 20:46:26 +0700

    update insert

--- Ex3/server/jobs/insert.js
+++ Ex3/server/jobs/insert.js
@@ -2,6 +2,7 @@ const { admin } = require('../firebase');
 const locationData = require('../data/location');
 const jobData = require('../data/jobs');
 const jobTypeData = require('../data/job-type');
+const jobDetailData = require('../data/jobDetail.json');
 
 const db = admin.database();
 const ref = db.ref();
@@ -9,11 +10,14 @@ const ref = db.ref();
 const locationRef = ref.child('location');
 const jobDataRef = ref.child('job');
 const jobTypeRef = ref.child('job-type');
+const jobDetailRef = ref.child('job-detail');
+
 
 Promise.all([
   locationRef.set(locationData),
   jobDataRef.set(jobData),
   jobTypeRef.set(jobTypeData),
+  jobDetailRef.set(jobDetailData),
 ])
   .then(() => {
     console.log('Insert database successfully');
