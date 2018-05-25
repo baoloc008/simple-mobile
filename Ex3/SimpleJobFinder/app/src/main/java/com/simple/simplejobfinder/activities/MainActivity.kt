1718f36218206136022fb7f95090775ca91ba989
Tho Nguyen Duc <ndtho8205@gmail.com>
2018-05-25 19:16:54 +0700

    Add load more function

--- Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/activities/MainActivity.kt
+++ Ex3/SimpleJobFinder/app/src/main/java/com/simple/simplejobfinder/activities/MainActivity.kt
@@ -9,46 +9,69 @@ import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter
 import com.simple.simplejobfinder.R
 import com.simple.simplejobfinder.adapters.JobListAdapter
 import com.simple.simplejobfinder.models.JobItem
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), RecyclerArrayAdapter.OnLoadMoreListener
 {
+
     private val _jobListAdapter = JobListAdapter(this)
-    private val offset = 0
-    private val limit = 10
+    private var _offset = 0
+    private val _limit = 10
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initJobListView()
-        loadFromFirebae(offset, limit)
+        loadFromFirebase(_offset, _limit)
     }
 
-    private fun loadFromFirebae(offset: Int, limit: Int)
+    override fun onLoadMore()
+    {
+        _offset += _limit + 1
+        loadFromFirebase(_offset, _limit)
+    }
+
+    private fun loadFromFirebase(offset: Int, limit: Int)
     {
         val startAt = offset.toString()
         val endAt = (offset + limit).toString()
         val mData = FirebaseDatabase.getInstance().reference
-        mData.child("job").orderByKey().startAt(startAt).endAt(endAt).addChildEventListener(object : ChildEventListener
-        {
-            override fun onCancelled(p0: DatabaseError) {}
-            override fun onChildMoved(p0: DataSnapshot, p1: String?) {}
-            override fun onChildChanged(p0: DataSnapshot, p1: String?) {}
-            override fun onChildRemoved( p0: DataSnapshot) {}
+        mData.child("job").orderByKey().startAt(startAt).endAt(endAt)
+                .addChildEventListener(object : ChildEventListener
+                                       {
+                                           override fun onCancelled(p0: DatabaseError)
+                                           {
+                                               _jobListAdapter.pauseMore()
+                                               rvJobList.showError()
+                                           }
 
-            override fun onChildAdded(p0: DataSnapshot, p1: String?)
-            {
-                val job = p0.getValue(JobItem::class.java)
-                job?.let {
-                    _jobListAdapter.add(it)
-                    _jobListAdapter.notifyDataSetChanged()
-                }
-            }
-        })
+                                           override fun onChildMoved(p0: DataSnapshot, p1: String?)
+                                           {
+                                           }
+
+                                           override fun onChildChanged(p0: DataSnapshot,
+                                                                       p1: String?)
+                                           {
+                                           }
+
+                                           override fun onChildRemoved(p0: DataSnapshot)
+                                           {
+                                           }
+
+                                           override fun onChildAdded(p0: DataSnapshot, p1: String?)
+                                           {
+                                               val job = p0.getValue(JobItem::class.java)
+                                               job?.let {
+                                                   _jobListAdapter.add(it)
+                                                   _jobListAdapter.notifyDataSetChanged()
+                                               }
+                                           }
+                                       })
     }
 
     private fun initJobListView()
@@ -66,20 +89,21 @@ class MainActivity : AppCompatActivity()
             startActivity(intent)
         }
 
-//        _jobListAdapter.setMore(R.layout.rv_more, this)
-//        _jobListAdapter.setError(R.layout.rv_job_list_error,
-//                                 object : RecyclerArrayAdapter.OnErrorListener
-//                                 {
-//                                     override fun onErrorShow()
-//                                     {
-//                                         _jobListAdapter.resumeMore()
-//                                     }
-//
-//                                     override fun onErrorClick()
-//                                     {
-//                                         _jobListAdapter.resumeMore()
-//                                     }
-//                                 })
+        _jobListAdapter.setMore(R.layout.rv_job_list_more, this)
+        _jobListAdapter.setNoMore(R.layout.rv_job_list_nomore)
+        _jobListAdapter.setError(R.layout.rv_job_list_error,
+                                 object : RecyclerArrayAdapter.OnErrorListener
+                                 {
+                                     override fun onErrorShow()
+                                     {
+                                         _jobListAdapter.resumeMore()
+                                     }
+
+                                     override fun onErrorClick()
+                                     {
+                                         _jobListAdapter.resumeMore()
+                                     }
+                                 })
 
         _jobListAdapter.clear()
 //        _jobListAdapter.pauseMore()
